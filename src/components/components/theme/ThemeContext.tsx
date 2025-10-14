acc10c14b477b2a2463e88c33f72ffa9f5a65ca4
jewel m <jeweltmichael@yahoo.com>
2025-10-14 11:11:59 +0530

    default theme to dark

--- src/components/components/theme/ThemeContext.tsx
+++ src/components/components/theme/ThemeContext.tsx
@@ -11,7 +11,7 @@ const ThemeContext = createContext<ThemeContextType | undefined>(undefined)
 
 export const ThemeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
   const [theme, setTheme] = useState<Theme>(
-    (localStorage.getItem('theme') as Theme) || 'light'
+    (localStorage.getItem('theme') as Theme) || 'dark'
   )
 
   useEffect(() => {
